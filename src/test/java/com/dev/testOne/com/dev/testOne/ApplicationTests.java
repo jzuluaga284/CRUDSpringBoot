9e996dc6b9e7d6904a338bd8520fc1df4117806d
Juan Zuluaga <jzuluaga284.gmail.com>
2023-07-12 10:43:38 -0500

    unit test

--- src/test/java/com/dev/testOne/com/dev/testOne/ApplicationTests.java
+++ src/test/java/com/dev/testOne/com/dev/testOne/ApplicationTests.java
@@ -1,13 +1,22 @@
 package com.dev.testOne.com.dev.testOne;
 
+import com.dev.testOne.com.dev.testOne.controllers.HelloWorldController;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
 
 @SpringBootTest
 class ApplicationTests {
 
+	@Autowired
+	private HelloWorldController helloWorldController;
 	@Test
-	void contextLoads() {
+	void helloWorldControllerTest() {
+
+		Assertions.assertEquals(HttpStatus.OK,helloWorldController.test());
 	}
 
 }
